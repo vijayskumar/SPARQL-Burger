d6ba8801a2a4bd4c139909ae8dcb2d985b7796c3
vijayskumar <vijayskumar@gmail.com>
2022-05-19 23:26:03 -0400

    Adding setup.py file for package installation

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="SPARQLBurger",
+    version="0.0.1",
+    author="GE Research",
+    author_email="v.kumar1@ge.com",
+    description="A modified version of the original SPARQLBurger query builder module",
+    url="URL",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[],
+    #python_requires='>=3.9',
+    package_data={}
+)
